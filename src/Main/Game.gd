2a2f754d124803ef532c5dcc3fdd8c379e72ad20
Arcaratus <byou64@gmail.com>
2020-10-14 23:15:57 -0400

    plz work

--- src/Main/Game.gd
+++ src/Main/Game.gd
@@ -11,30 +11,30 @@ var reticle = load("res://assets/art/ui/reticle.png")
 
 
 func _init():
-	OS.min_window_size = OS.window_size
-	OS.max_window_size = OS.get_screen_size()
+    OS.min_window_size = OS.window_size
+    OS.max_window_size = OS.get_screen_size()
 
 
 func _notification(what):
-	if what == NOTIFICATION_WM_QUIT_REQUEST:
-		# We need to clean up a little bit first to avoid Viewport errors.
-		if name == "Splitscreen":
-			$Black/SplitContainer/ViewportContainer1.free()
-			$Black.queue_free()
+    if what == NOTIFICATION_WM_QUIT_REQUEST:
+        # We need to clean up a little bit first to avoid Viewport errors.
+        if name == "Splitscreen":
+            $Black/SplitContainer/ViewportContainer1.free()
+            $Black.queue_free()
 
 
 func _ready():
-	Input.set_custom_mouse_cursor(reticle)
+    Input.set_custom_mouse_cursor(reticle)
 
 
 #func _unhandled_input(event):
 #    if event.is_action_pressed("toggle_fullscreen"):
 #        OS.window_fullscreen = not OS.window_fullscreen
 #        get_tree().set_input_as_handled()
-	# The GlobalControls node, in the Stage scene, is set to process even
-	# when the game is paused, so this code keeps running.
-	# To see that, select GlobalControls, and scroll down to the Pause category
-	# in the inspector.
+    # The GlobalControls node, in the Stage scene, is set to process even
+    # when the game is paused, so this code keeps running.
+    # To see that, select GlobalControls, and scroll down to the Pause category
+    # in the inspector.
 #    elif event.is_action_pressed("toggle_pause"):
 #        var tree = get_tree()
 #        tree.paused = not tree.paused
@@ -43,7 +43,7 @@ func _ready():
 #        else:
 #            _pause_menu.close()
 #        get_tree().set_input_as_handled()
-	
+    
 #    elif event.is_action_pressed("splitscreen"):
 #        if name == "Splitscreen":
 #            # We need to clean up a little bit first to avoid Viewport errors.
